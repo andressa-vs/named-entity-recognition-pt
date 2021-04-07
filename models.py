a481c2744df2a82b12bbaf452089ef8bae9f879e
andressa-vs <37421268+andressa-vs@users.noreply.github.com>
2021-04-07 13:13:02 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,40 +1,39 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 17 14:17:13 2021
-
-@author: andressa
-"""
-
 from keras.models import Model
-from keras.layers import Dense, Input, Embedding, LSTM, Bidirectional, TimeDistributed, Dropout
-import keras
+from keras.layers import Dense, Input, Embedding, LSTM, Bidirectional, TimeDistributed, Dropout, Concatenate
+from keras.initializers import RandomUniform
 from transformers import TFBertModel
 import numpy as np
 from tensorflow.keras.optimizers import Adam, schedules
 import tensorflow.compat.v1 as tf
+from keras_crf import CRF
 
-class Blstm(object):
+
+class BlstmForNER(object):
     
-    def __init__(self, model_name, scenario, labels, max_len=128, doc_stride=64, lstm_layer=512,
-                 features_dim=75, number_bert_layers=4, trainable_embedding=False, 
-                 features=None, dropout=0.6):
-        self.model_name = model_name
-        self.scenario = scenario
+    def __init__(self, bert_model_path, labels, 
+                 max_len=128, 
+                 lstm_layer=512,
+                 features_dim=75, 
+                 number_bert_layers=4, 
+                 bert_is_trainable=False, 
+                 features=None, 
+                 dropout=0.6):
+        
+        self.bert_model_path = bert_model_path
         self.labels = labels
         self.max_len = max_len
-        self.doc_stride = doc_stride
         self.lstm_layer = lstm_layer
         self.features_dim = features_dim
         self.number_bert_layers = number_bert_layers
-        self.trainable_embedding = trainable_embedding
+        self.bert_is_trainable = bert_is_trainable
         self.features = features
         self.dropout = dropout
         self.from_pt = False
         self.output_hidden_states = False
-
         
-        if self.model_name == 'neuralmind/bert-base-portuguese-cased':
+        bert_portuguese_cased = 'neuralmind/bert-base-portuguese-cased'
+        
+        if self.bert_model_path == bert_portuguese_cased:
             self.from_pt = True
         
         if self.number_bert_layers > 1:
@@ -43,14 +42,10 @@ class Blstm(object):
         self.lab_to_ind = self.labels_to_index()
         self.ind_to_lab = self.index_to_labels()
         
-        self.generate_model()
-
         
     def __repr__(self):
         s = ("model name: {}\n"
-             "scenario: {}\n"
              "max len: {}\n"
-             "doc stride: {}\n"
              "lstm layer: {}\n"
              "features dim: {}\n"
              "number bert layers: {}\n"
@@ -59,15 +54,16 @@ class Blstm(object):
              "labels: {}\n"
              "from pytorch: {}\n"
              "return hidden states: {}\n"
-             "dropout: {}").format(self.model_name, self.scenario,
-                                         self.max_len, self.doc_stride,
-                                         self.lstm_layer, self.features_dim,
-                                         self.number_bert_layers,
-                                         self.trainable_embedding,
-                                         self.features.keys(), self.labels,
-                                         self.from_pt, self.return_hidden_states,
-                                         self.dropout)
+             "dropout: {}").format(self.bert_model_path, 
+                                   self.max_len, 
+                                    self.lstm_layer, self.features_dim,
+                                    self.number_bert_layers,
+                                    self.bert_is_trainable,
+                                    self.features.keys(), self.labels,
+                                    self.from_pt, self.return_hidden_states,
+                                    self.dropout)
         return s
+    
     
     def bert_embedding_layer(self):
         
@@ -82,25 +78,25 @@ class Blstm(object):
                                           name="segment_ids")
         
         sequence_inputs = [input_word_ids, input_mask, segment_ids]
-        bert_layer = TFBertModel.from_pretrained(self.model_name,from_pt=from_pt)
+        bert_layer = TFBertModel.from_pretrained(self.bert_model_path,from_pt=from_pt)
         bert_layer.config.output_hidden_states = output_hidden_states
         
         if output_hidden_states:
           hidden_layers = bert_layer(sequence_inputs)[2]
           selected_layers = hidden_layers[-self.number_bert_layers:]
-          sequence_output = tf.keras.layers.Concatenate()(selected_layers)
+          sequence_output = Concatenate()(selected_layers)
         else:
           sequence_output,_ = bert_layer(sequence_inputs)
         
         bert_model = Model(inputs=sequence_inputs, outputs=sequence_output)
         
-        bert_model.trainable = self.trainable_embedding
+        bert_model.trainable = self.bert_is_trainable
         
         return bert_model
     
     def numeric_feature_layer(self, number_features, featureName=None):
     
-        initalization = keras.initializers.RandomUniform(minval=-0.5, maxval=0.5)
+        initalization = RandomUniform(minval=-0.5, maxval=0.5)
         
         inputs = Input(shape=(self.max_len,), dtype=tf.int32, name=featureName)
         embeddingLayer = Embedding(number_features, self.features_dim, input_length=self.max_len,
@@ -132,7 +128,7 @@ class Blstm(object):
         
       return layers
   
-    def generate_model(self):
+    def build_model(self):
         
         self.features_models = self.features_layers()
         self.bert_model = self.bert_embedding_layer()
@@ -143,7 +139,7 @@ class Blstm(object):
         else:
           models = [self.bert_model] + self.features_models
           inputs = [model.input for model in models]
-          embedding_layer = keras.layers.Concatenate(axis=-1, name='concat_layer')([model.output for model in models])
+          embedding_layer = Concatenate(axis=-1, name='concat_layer')([model.output for model in models])
           
         blstm = Bidirectional(LSTM(self.lstm_layer, return_sequences=True))(embedding_layer)
         dropout_layer = Dropout(self.dropout)(blstm)
@@ -188,12 +184,54 @@ class Blstm(object):
                     
         preds_mask = ((evaluate_labels != [x_label]) & (is_max_context))
         
-        real_preds = [lab if lab != x_label else o_label for lab in np.argmax(logits[preds_mask], axis=-1)]
-        real_labels = evaluate_labels[preds_mask]
+        real_preds_idx = [lab if lab != x_label else o_label for lab in np.argmax(logits[preds_mask], axis=-1)]
+        real_labels_idx = evaluate_labels[preds_mask]
         
-        assert len(real_preds) == len(real_labels)
+        assert len(real_preds_idx) == len(real_labels_idx)
         
-        real_preds_tags = self.convert_to_labels(real_preds)
-        real_labels_tags = self.convert_to_labels(real_labels)
+        real_preds_tags = self.convert_to_labels(real_preds_idx)
+        real_labels_tags = self.convert_to_labels(real_labels_idx)
           
         return real_preds_tags, real_labels_tags
+
+class BlstmForNerCRF(BlstmForNER):
+    
+    def __init__(self, labels, **kwargs):
+        super().__init__(labels, **kwargs)
+        self.crf = CRF(len(self.labels))
+    
+    def build_model(self):
+        
+        self.features_models = self.features_layers()
+        self.bert_model = self.bert_embedding_layer()
+    
+        if self.features_models == []:
+          inputs = self.bert_model.input
+          embedding_layer = self.bert_model.output
+        else:
+          models = [self.bert_model] + self.features_models
+          inputs = [model.input for model in models]
+          embedding_layer = Concatenate(axis=-1, name='concat_layer')([model.output for model in models])
+          
+        blstm = Bidirectional(LSTM(self.lstm_layer, return_sequences=True))(embedding_layer)
+        dropout_layer = Dropout(self.dropout)(blstm)
+        outputs = self.crf(dropout_layer)
+        
+        self.blstm_model = Model(inputs=inputs, outputs=outputs)
+        print(self.blstm_model.summary())
+        
+    def train(self, train_inputs, train_labels, validation_data=(), num_epochs=50, 
+              learning_rate=1e-3, batch=32, decay_steps=1.0, decay_rate=1e-5):
+        
+        schedule = schedules.InverseTimeDecay(learning_rate, decay_steps, 
+                                              decay_rate, staircase=False, 
+                                              name=None)
+        optimizer = Adam(learning_rate=schedule)
+        
+        self.blstm_model.compile(optimizer=optimizer, 
+                                 loss=self.crf.neg_log_likelihood,
+                                 metrics=[self.crf.accuracy])
+
+        self.blstm_model.fit(train_inputs, train_labels,
+            validation_data= validation_data, epochs= num_epochs,
+            batch_size=batch)
