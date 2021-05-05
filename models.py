b5daf37e68cf66b07cd596bbc5ed3cf034fdfc1e
andressa-vs <37421268+andressa-vs@users.noreply.github.com>
2021-05-05 13:50:38 -0300

    Update models.py

--- models.py
+++ models.py
@@ -242,7 +242,7 @@ class BlstmForNerCRF(BlstmForNer):
                                               decay_rate, 
                                               staircase=False, 
                                               name=None)
-        optimizer = Adam(learning_rate=learning_rate)
+        optimizer = Adam(learning_rate=schedule)
         
         self.blstm_model.compile(optimizer=optimizer, 
                                  loss=self.crf.neg_log_likelihood,
