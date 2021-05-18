377b8237bc95a8f81213e4228c55fdef9b80e90b
andressa-vs <37421268+andressa-vs@users.noreply.github.com>
2021-05-18 10:54:22 -0300

    Update models.py

--- models.py
+++ models.py
@@ -224,7 +224,7 @@ class BlstmForNerCRF(BlstmForNer):
           embedding_layer = Concatenate(axis=-1, name='concat_layer')([model.output for model in models])
 
           
-        blstm = Bidirectional(LSTM(self.lstm_layer, return_sequences=True))(embedding_layer)
+        blstm = Bidirectional(LSTM(self.lstm_layer, return_sequences=True, dropout=self.dropout))(embedding_layer)
         time_dist = TimeDistributed(Dense(self.lstm_layer, activation='relu'))(blstm)
         normalization = LayerNormalization(1)(time_dist)
         outputs = self.crf(normalization)
@@ -260,7 +260,7 @@ class BlstmForNerCRF(BlstmForNer):
                     
         preds_mask = ((evaluate_labels != [x_label]) & (is_max_context))
         
-        real_preds_idx = [lab if lab != x_label else o_label for lab in np.argmax(logits[preds_mask], axis=-1)]
+        real_preds_idx = [lab if lab != x_label else o_label for lab in logits[preds_mask]]
         real_labels_idx = evaluate_labels[preds_mask]
         
         assert len(real_preds_idx) == len(real_labels_idx)
